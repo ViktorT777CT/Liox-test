2ea4dd039b9a811dbdeff3569b5456818ab186bd
ViktorT777CT <p_viktor91@mail.ru>
2024-05-06 19:57:38 +0600

    start

---
+++ database/migrations/2024_05_06_133149_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('theme');
+            $table->text('description');
+            $table->string('client');
+            $table->string('email');
+            $table->string('link_to_the_file');
+            $table->dateTime('create_task');
+            $table->dateTime('start_task');
+            $table->dateTime('end_task');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
